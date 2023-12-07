650c71ba516d4a200fe08ce854ae1bbcf87490aa
tmandali <tmandali@gmail.com>
2023-12-08 00:46:13 +0300

    synmq service

--- src/ClientApp/Program.cs
+++ src/ClientApp/Program.cs
@@ -1,14 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Google.Protobuf;
+using Grpc.Core;
 using Grpc.Net.Client;
-using ServiceApp;
+using Pars.Extensions.SyncMq;
 
-Console.WriteLine("Hello, World!");
+//using var channel = GrpcChannel.ForAddress("https://grpcerptest.azurewebsites.net/");
+using var channel = GrpcChannel.ForAddress("https://localhost:7082"); //
 
-using var channel = GrpcChannel.ForAddress("https://grpcerptest.azurewebsites.net/"); //https://localhost:7082
-var client = new Greeter.GreeterClient(channel);
+var client = new SyncMq.SyncMqClient(channel);
 
-var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
-Console.WriteLine("Greeting: " + reply.Message);
+var corelationId = UnsafeByteOperations.UnsafeWrap(Guid.NewGuid().ToByteArray());
+
+var message1 = new MessageBroker() {
+    MessageId = Guid.NewGuid().ToString(),
+    Topic = "/topic",
+    Data = ByteString.Empty, 
+    Headers = {
+        { "DataAreaId", ByteString.CopyFromUtf8("TRLC") },
+        { "CorelationId", corelationId }
+    }
+};
+
+var message2 = new MessageBroker()
+{
+    MessageId = Guid.NewGuid().ToString(),
+    Topic = "/topic",
+    Data = ByteString.Empty,
+    Headers = {
+        { "DataAreaId", ByteString.CopyFromUtf8("TRLC") },
+        { "CorelationId", corelationId }
+    }
+};
+
+var xx = new HashSet<MessageBroker>
+{
+    message1,
+    message2
+};
+
+using var send = client.SendMessage();
+
+await send.RequestStream.WriteAsync(message1);
+await send.RequestStream.WriteAsync(message2);
+
+var i = 0;
+await foreach (var response in send.ResponseStream.ReadAllAsync())
+{
+    if (++i == 2)
+        await send.RequestStream.CompleteAsync();    
+}
+
 
 Console.WriteLine("Shutting down");
 Console.WriteLine("Press any key to exit...");
---
+++ src/ServiceApp/Services/SyncMqService.cs
@@ -0,0 +1,19 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Pars.Extensions.SyncMq;
+
+namespace ServiceApp.Services;
+
+public class SyncMqService : SyncMq.SyncMqBase
+{
+    public override async Task SendMessage(IAsyncStreamReader<MessageBroker> requestStream, IServerStreamWriter<Result> responseStream, ServerCallContext context)
+    {
+        var i = 0;   // begin tran
+        await foreach (var request in requestStream.ReadAllAsync())
+        {
+            i++;
+            await responseStream.WriteAsync(new Result() { Topic = request.Topic, MessageId = request.MessageId, Key = request.Key });
+        }
+        // commit tran
+    }
+}
