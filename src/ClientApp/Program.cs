93dd579ebebfe04e57223df16ef3d28a442661c5
tmandali <tmandali@gmail.com>
2023-12-08 22:56:31 +0300

    get

--- src/ClientApp/Program.cs
+++ src/ClientApp/Program.cs
@@ -8,6 +8,18 @@ using var channel = GrpcChannel.ForAddress("https://localhost:7082"); //
 
 var client = new SyncMq.SyncMqClient(channel);
 
+var messages = client.GetMessages(); 
+
+await messages.RequestStream.WriteAsync(new() { Topic = "/topic", Subscriber = "ClientApp" });
+await foreach (var message in messages.ResponseStream.ReadAllAsync())
+{
+    if (!message.Next)
+        await messages.RequestStream.CompleteAsync();
+    else
+        await messages.RequestStream.WriteAsync(new() { Topic = "/topic", Subscriber = "ClientApp" });
+}
+
+// send
 var corelationId = UnsafeByteOperations.UnsafeWrap(Guid.NewGuid().ToByteArray());
 
 var message1 = new MessageBroker() {
@@ -42,13 +54,12 @@ using var send = client.SendMessage();
 await send.RequestStream.WriteAsync(message1);
 await send.RequestStream.WriteAsync(message2);
 
-var i = 0;
+var r = 0;
 await foreach (var response in send.ResponseStream.ReadAllAsync())
 {
-    if (++i == 2)
+    if (++r == 2)
         await send.RequestStream.CompleteAsync();    
 }
-
 
 Console.WriteLine("Shutting down");
 Console.WriteLine("Press any key to exit...");
--- src/ServiceApp/Services/SyncMqService.cs
+++ src/ServiceApp/Services/SyncMqService.cs
@@ -1,19 +1,41 @@
-﻿using Google.Protobuf.WellKnownTypes;
-using Grpc.Core;
+﻿using Grpc.Core;
 using Pars.Extensions.SyncMq;
 
 namespace ServiceApp.Services;
 
 public class SyncMqService : SyncMq.SyncMqBase
 {
+    private readonly ILogger<SyncMqService> _logger;
+
+    public SyncMqService(ILogger<SyncMqService> logger)
+    {
+        _logger = logger;
+    }
+
     public override async Task SendMessage(IAsyncStreamReader<MessageBroker> requestStream, IServerStreamWriter<Result> responseStream, ServerCallContext context)
     {
-        var i = 0;   // begin tran
         await foreach (var request in requestStream.ReadAllAsync())
         {
-            i++;
             await responseStream.WriteAsync(new Result() { Topic = request.Topic, MessageId = request.MessageId, Key = request.Key });
         }
-        // commit tran
+    }
+
+    public override async Task GetMessages(IAsyncStreamReader<Request> request, IServerStreamWriter<MessageBroker> responseStream, ServerCallContext context)
+    {
+        int i = 0;
+        int msg = 0;
+        _logger.LogInformation("Begintran {0}",i);
+        await foreach (var item in request.ReadAllAsync())
+        {
+            var message = new MessageBroker() { Topic = item.Topic, Next = ++msg <= 3 };
+
+            await responseStream.WriteAsync(message);
+            if (item.Commit)
+            {
+                _logger.LogInformation("Committran {0}", i);
+                _logger.LogInformation("Begintran {0}", ++i);
+            }
+        }
+        _logger.LogInformation("Committran {0}", i);
     }
 }
