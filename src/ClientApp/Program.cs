7f2d2855904e0024dd91e396d842d295f10f95ac
tmandali <tmandali@gmail.com>
2023-12-14 23:44:54 +0300

    defe

--- src/ClientApp/Program.cs
+++ src/ClientApp/Program.cs
@@ -4,7 +4,7 @@ using Grpc.Net.Client;
 using Pars.Messaging;
 
 using var channel = GrpcChannel.ForAddress("http://localhost:5121"
-    , new()
+    , new() 
     {
         HttpHandler = new HttpClientHandler()
         {
@@ -13,63 +13,85 @@ using var channel = GrpcChannel.ForAddress("http://localhost:5121"
     }
 );
 
+Console.WriteLine("Subsriber begin"); 
+
 var client = new SyncMqGateway.SyncMqGatewayClient(channel);
-await client.Subscribe("subscriber", new[] { "/topic", "/topic1" });
-
-Console.WriteLine("Subsriber begin");
-var metadata = new Metadata
+using var subscriber = client.CreateSubscriptionStream("subscriber", new[]{ "/topic", "/topic1" });
+await foreach (var message in subscriber.ReadAllAsync())
 {
-    new Metadata.Entry("subscriber", "/subscriber"),
-    new Metadata.Entry("topic1", "/topic"),
-    new Metadata.Entry("topic2", "/topicx"),
-};
-
-using var receive = client.Subscribe(metadata);
-while (await receive.ResponseStream.MoveNext() && receive.ResponseStream.Current is not null)
-{
-    string message_id = receive.ResponseStream.Current.MessageId;
-    string topic = receive.ResponseStream.Current.Topic;
-    Console.WriteLine("{0} {1} begin", topic, message_id);
-
-    using MemoryStream data = new();
-    do
-    {
-        data.Write(receive.ResponseStream.Current.Data.Span);
-    } while (!(receive.ResponseStream.Current?.MessageEof ?? true) && await receive.ResponseStream.MoveNext());
-
-    await receive.RequestStream.WriteAsync(new Request() { Commit = true });
-    Console.WriteLine("{0} {1} end received bytes {2:N0}", topic, message_id, data.Length);
+    Console.WriteLine("{0} {1} message received, bytes {2:N0}", message.Topic, message.MessageId, message.Data.Length);
 }
-Console.WriteLine("Subsriber end");
 
+//var metadata = new Metadata
+//{
+//    new Metadata.Entry("subscriber", "/subscriber"),
+//    new Metadata.Entry("topic1", "/topic"),
+//    new Metadata.Entry("topic2", "/topicx"),
+//};
+
+//using var receive = client.Subscribe(metadata);
+//while (await receive.ResponseStream.MoveNext() && receive.ResponseStream.Current is not null)
+//{
+//    string message_id = receive.ResponseStream.Current.MessageId;
+//    string topic = receive.ResponseStream.Current.Topic;
+//    Console.WriteLine("{0} {1} begin", topic, message_id);
+
+//    using MemoryStream data = new();
+//    do
+//    {
+//        data.Write(receive.ResponseStream.Current.Data.Span);
+//    } while (!(receive.ResponseStream.Current?.MessageEof ?? true) && await receive.ResponseStream.MoveNext());
+
+//    await receive.RequestStream.WriteAsync(new Request() { Commit = true });
+//    Console.WriteLine("{0} {1} end received bytes {2:N0}", topic, message_id, data.Length);
+//}
+Console.WriteLine("Subsriber end");
 Console.ReadLine();
 
-const int ChunkSize = 1024 * 64; // 64 KB
-
-using var publisher = client.Publish();
+using var publisher = client.CreatePublicationStream();
 foreach (var file in new[] {
     @"C:\Users\tmand\Pictures\Ekran görüntüsü 2023-06-15 141121.png",
     @"C:\Users\tmand\Pictures\Ekran görüntüsü 2023-09-22 101652.png",
     @"C:\Users\tmand\Pictures\Ekran görüntüsü 2023-09-22 101712.png" })
 {
-    var buffer = new byte[ChunkSize];
-    var message = new MessageBroker() { MessageId = Guid.NewGuid().ToString(), Topic = "/topic" };
-
     await using var readStream = File.OpenRead(file);
-    var count = await readStream.ReadAsync(buffer);
-    while (count > 0)
-    {
-        message.Data = UnsafeByteOperations.UnsafeWrap(buffer.AsMemory(0, count));
-        count = await readStream.ReadAsync(buffer);
-        message.MessageEof = count == 0;
-        await publisher.RequestStream.WriteAsync(message);
+    var message = new MessageBroker() {
+        MessageId = Guid.NewGuid().ToString(),
+        Topic = "/topic",
+        Data = ByteString.FromStream(readStream)
+    };
 
-        if (message.MessageEof)
-            if (await publisher.ResponseStream.MoveNext())
-                Console.WriteLine("published {0}", publisher.ResponseStream.Current.EventId);
-    }
+    await publisher.WriteAsync(message);
 }
-await publisher.RequestStream.CompleteAsync();
+await publisher.CompleteAsync();
+
+
+//const int ChunkSize = 1024 * 64; // 64 KB
+
+//using var publisher = client.Publish();
+//foreach (var file in new[] {
+//    @"C:\Users\tmand\Pictures\Ekran görüntüsü 2023-06-15 141121.png",
+//    @"C:\Users\tmand\Pictures\Ekran görüntüsü 2023-09-22 101652.png",
+//    @"C:\Users\tmand\Pictures\Ekran görüntüsü 2023-09-22 101712.png" })
+//{
+//    var buffer = new byte[ChunkSize];
+//    var message = new MessageBroker() { MessageId = Guid.NewGuid().ToString(), Topic = "/topic" };
+
+//    await using var readStream = File.OpenRead(file);
+//    var count = await readStream.ReadAsync(buffer);
+//    while (count > 0)
+//    {
+//        message.Data = UnsafeByteOperations.UnsafeWrap(buffer.AsMemory(0, count));
+//        count = await readStream.ReadAsync(buffer);
+//        message.MessageEof = count == 0;
+//        await publisher.RequestStream.WriteAsync(message);
+
+//        if (message.MessageEof)
+//            if (await publisher.ResponseStream.MoveNext())
+//                Console.WriteLine("published {0}", publisher.ResponseStream.Current.EventId);
+//    }
+//}
+//await publisher.RequestStream.CompleteAsync();
 
 Console.WriteLine("Shutting down");
 Console.WriteLine("Press any key to exit...");
--- src/ServiceApp/Program.cs
+++ src/ServiceApp/Program.cs
@@ -1,3 +1,4 @@
+using Grpc.Net.Client.Balancer;
 using ServiceApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,3 +13,9 @@ var app = builder.Build();
 app.MapGrpcService<SyncMqService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 app.Run();
+
+var factory = new StaticResolverFactory(addr => new[]
+{
+    new BalancerAddress("localhost", 80),
+    new BalancerAddress("localhost", 81)
+});
